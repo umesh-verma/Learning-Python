d7ce5d7375f9fb84ca9509c963847bbd870017ed
root <root@localhost.localdomain>
2019-11-23 17:15:36 +0530

    variables.py - variable and basic airtmetics

---
+++ Introduction - basics/variables.py
@@ -0,0 +1,51 @@
+# single line comment starts with # and this is a comment
+'''    this is
+a multi line
+comment 
+
+'''
+
+# no data type defination required
+#directly declare variable
+
+#addition
+num=1+2
+print(num)
+#output 3 int
+
+num=1.0+2
+print(num)
+#output 3.0  float
+
+#subtraction
+num=1-2
+print(num)
+# 3 int
+
+#multiplication
+num=1*2
+print(num)
+# 2 int
+
+num=1.0*2
+print(num)
+# 2.0
+
+#division
+num=1/2
+print(num)
+# 0.5
+
+num=1//2
+print(num)
+# 0  returns integer quotient
+
+#modulous
+num=1%2
+print(num)
+# 1  modulo
+
+#power
+num = 2**2
+print(num)
+#output 4
