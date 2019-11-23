6c20b1a76d5843c1901f28f11a37668569bf1c57
root <root@localhost.localdomain>
2019-11-23 17:19:33 +0530

    complying flake8

--- Introduction - basics/FirstProgram.py
+++ Introduction - basics/FirstProgram.py
@@ -1,4 +1,5 @@
 print("hello world")
 
+
 #  output :-
 #  hello world
--- Introduction - basics/variables.py
+++ Introduction - basics/variables.py
@@ -1,51 +1,51 @@
 # single line comment starts with # and this is a comment
 '''    this is
 a multi line
-comment 
+comment
 
 '''
 
 # no data type defination required
-#directly declare variable
+# directly declare variable
 
-#addition
-num=1+2
+# addition
+num = 1+2
 print(num)
-#output 3 int
+# output 3 int
 
-num=1.0+2
+num = 1.0+2
 print(num)
-#output 3.0  float
+# output 3.0  float
 
-#subtraction
-num=1-2
+# subtraction
+num = 1-2
 print(num)
 # 3 int
 
-#multiplication
-num=1*2
+# multiplication
+num = 1*2
 print(num)
 # 2 int
 
-num=1.0*2
+num = 1.0*2
 print(num)
 # 2.0
 
-#division
-num=1/2
+# division
+num = 1/2
 print(num)
 # 0.5
 
-num=1//2
+num = 1//2
 print(num)
 # 0  returns integer quotient
 
-#modulous
-num=1%2
+# modulous
+num = 1 % 2
 print(num)
 # 1  modulo
 
-#power
-num = 2**2
+# power
+num = 2 ** 2
 print(num)
-#output 4
+# output 4
